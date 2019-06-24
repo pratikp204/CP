14845f63255883220795c692589d754c8abd00ba
Pratik Patil <31205021+pratikp204@users.noreply.github.com>
2019-06-24 06:03:34 +0530

    removed 8,10,11

--- spoj/ONP.cpp
+++ spoj/ONP.cpp
@@ -5,11 +5,7 @@
 using namespace std;
 
 #define FOR(i,a,b) for(int i=a;i<b;i++)
-#define FORD(i,a,b) for(int i=a;i>=b;i--)
-#define REP(i,n) FOR(i,0,n)
 #define PB push_back
-#define ITER(i,a) for( typeof(a.begin()) i=a.begin();i!=a.end();i++)	
-#define mod 1000000007
 #define MAXN 1000010
 #define MP make_pair
 #define INF mod
